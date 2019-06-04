93c2530d2b4c669782e9eebdc469c61d0740c442
佳文 <nbsawz@gmail.com>
2019-06-04 10:00:07 +0800

    add more test case

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,15 @@
-﻿"use strict";
+﻿import firstletter from "./dict/firstletter";
 
-import firstletter from "./dict/firstletter";
-
-export default function(str: string) {
-  try {
-    if (!str || /^ +$/g.test(str)) return "";
-    var result = [];
-    for (let i = 0; i < str.length; i++) {
-      var unicode = str.charCodeAt(i);
-      let char = str.charAt(i);
-      if (unicode >= 19968 && unicode <= 40869)
-        char = firstletter.charAt(unicode - 19968);
-      result.push(char);
-    }
-    return result.join("");
-  } catch (e) {
-    console.error(`[pyfl] something went wrong ... ${e}`);
-    console.error(`[pylf] params: ${firstletter}`);
-    return str;
+export default function(raw: any) {
+  const str = `${raw}`
+  if (!str || /^ +$/g.test(str)) return "";
+  let result = [];
+  for (let i = 0; i < str.length; i++) {
+    let unicode = str.charCodeAt(i);
+    let char = str.charAt(i);
+    if (unicode >= 19968 && unicode <= 40869)
+      char = firstletter.charAt(unicode - 19968);
+    result.push(char);
   }
+  return result.join("");
 }
